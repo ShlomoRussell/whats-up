a97c29f26309a11380f294d0051ef4f2583fd379
bearddude1 <baapteshuva@gmail.com>
2022-05-18 21:56:04 +0300

    static

--- server/index.js
+++ server/index.js
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 5782
 
+app.use('/', express.static('public'));
+
 app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
