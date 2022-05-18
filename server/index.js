4c1104c60c9e0bb062c884430c87d3c55ed8591b
bearddude1 <baapteshuva@gmail.com>
2022-05-19 02:06:37 +0300

    update and format

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
-
 const app = express();
-const webSocket = require('./web-socket');
+const webSocket = require("./web-socket");
 const dotenv = require("dotenv").config();
 
-
 const PORT = process.env.PORT;
-
 
 app.use("/", express.static("public"));
 
 const server = app.listen(PORT, () =>
   console.log(`server started on port ${PORT}`)
 );
-webSocket(server)
+webSocket(server);
--- server/web-socket.js
+++ server/web-socket.js
@@ -1,37 +1,35 @@
 const webSocket = require("ws");
-const queryString = require('query-string');
-
+const queryString = require("query-string");
 
 const attachExpressToWebSocket = async (expressServer) => {
-
   const webSocketServer = new webSocket.Server({
     noServer: true,
-    path: "/websockets",
+    path: "/",
   });
-    
-    
- expressServer.on("upgrade", (request, socket, head) => {
-   webSocketServer.handleUpgrade(request, socket, head, (websocket) => {
-     webSocketServer.emit("connection", websocket, request);
-   });
-     
-     
-     webSocketServer.on('connection', (webSocketConnection, connectionRequest) => {
-         const [_path, params] = connectionRequest?.url?.split('?');
-         const connectionParams = queryString.parse(params);
-         console.log(connectionParams);
-         
 
+  expressServer.on("upgrade", (request, socket, head) => {
+    webSocketServer.handleUpgrade(request, socket, head, (websocket) => {
+      webSocketServer.emit("connection", websocket, request);
+    });
+  });
 
-         webSocketConnection.on('message', (message) => {
-             const parsedMessage = JSON.parse(message);
-             console.log(parsedMessage);
-             webSocketConnection.send(JSON.stringify({message:'There will be gold in the hills.'}))
-         })
-     })
- });
+  webSocketServer.on("connection", (webSocketConnection, connectionRequest) => {
+    const [_path, params] = connectionRequest?.url?.split("?");
+    const connectionParams = queryString.parse(params);
+    console.log(connectionParams);
+
+    webSocketConnection.on("message", (message) => {
+      const parsedMessage = JSON.parse(message);
+      console.log(parsedMessage);
+      webSocketConnection.send(
+        JSON.stringify({
+          message: `Your message of ${parsedMessage.test} has been received`,
+        })
+      );
+    });
+  });
+
   return webSocketServer;
 };
-
 
 module.exports = attachExpressToWebSocket;
