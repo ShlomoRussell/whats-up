b913e44ec924ce72667fb54cbb64dc05313feee1
Amicht <vicolicht@gmail.com>
2022-05-19 18:10:37 +0300

    download jwt and fixing routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const ErrorModel = require("./models/error-model");
 const apiCtrl = require("./controllers/api-controller");
 const app = express();
 const webSocket = require("./web-socket");
+const authctrl = require("./controllers/auth-controller");
 require("dotenv").config();
 
 app.use(express.urlencoded({extended:true}));
@@ -13,6 +14,7 @@ const PORT = process.env.PORT;
 
 app.use("/", express.static("public"));
 app.use('/api/users', apiCtrl);
+app.use('/auth', authctrl);
 
 // for non existing routes or url type error
 app.use('*', (req,res,next)=> next(new ErrorModel(404, 'Route not found'))); 
