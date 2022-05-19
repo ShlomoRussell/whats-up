7c31acb19b644b6f379231d880d8bdee7363bb8a
bearddude1 <baapteshuva@gmail.com>
2022-05-19 20:03:06 +0300

    added socket.io

--- server/index.js
+++ server/index.js
@@ -1,25 +1,33 @@
 const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+const { Server } = require("socket.io"); 
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+  console.log(`You're connect with the id:${socket.id}`);
+});
+
 const errorHandler = require("./middleware/error-handler");
 const ErrorModel = require("./models/error-model");
 const apiCtrl = require("./controllers/api-controller");
-const app = express();
-const webSocket = require("./web-socket");
+
+
 require("dotenv").config();
 
-app.use(express.urlencoded({extended:true}));
+app.use('/',express.static('../client'))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const PORT = process.env.PORT;
 
-app.use("/", express.static("public"));
-app.use('/api/users', apiCtrl);
+
+app.use("/api/users", apiCtrl);
 
 // for non existing routes or url type error
-app.use('*', (req,res,next)=> next(new ErrorModel(404, 'Route not found'))); 
+app.use("*", (req, res, next) => next(new ErrorModel(404, "Route not found")));
 app.use(errorHandler);
 
-const server = app.listen(PORT, () =>
-  console.log(`server started on port ${PORT}`)
-);
 
-webSocket(server);
+server.listen(PORT, () => console.log(`server started on port ${PORT}`));
