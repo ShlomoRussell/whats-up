53a57caecab55100f13cc54c09815fe58d698cb4
Amicht <vicolicht@gmail.com>
2022-05-20 01:12:24 +0300

    prettier index file :)

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const errorHandler = require("./middleware/error-handler");
+const ErrorModel = require("./models/error-model");
+const apiCtrl = require("./controllers/api-controller");
+const authctrl = require("./controllers/auth-controller");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
 const app = express();
 
 const server = require("http").createServer(app);
@@ -19,26 +26,12 @@ io.on("connection", (socket) => {
   console.log(`You're connect with the id:${socket.id}`);
 });
 
-const errorHandler = require("./middleware/error-handler");
-const ErrorModel = require("./models/error-model");
-const apiCtrl = require("./controllers/api-controller");
-
-const authctrl = require("./controllers/auth-controller");
-require("dotenv").config();
-
-//app.use('/',express.static('../client'))
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-const PORT = process.env.PORT 
-
+app.use(express.json()); 
 
 app.use("/api/users", apiCtrl);
 app.use('/auth', authctrl);
-
-// for non existing routes or url type error
 app.use("*", (req, res, next) => next(new ErrorModel(404, "Route not found")));
 app.use(errorHandler);
-
 
 server.listen(PORT, () => console.log(`server started on port: ${PORT}`));
