21e0d8de78b81004fc881273b03213ac6c644700
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-05-28 22:57:37 +0300

    added private messaging abilities

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import { socket } from "./socket/connect-socket";
-import {createRef, useState} from 'react'
+import React, { createRef, useState } from "react";
 function App() {
   const [messages, setMessages] = useState([]);
   socket.on("receive-message", (message) => {
-    console.log(message)
-    setMessages([...messages,message])
-  })
-  const messageRef= createRef()
+    console.log(message);
+    setMessages([...messages, message]);
+  });
+  const messageRef = createRef();
+   const idRef = createRef();
   const handleClick = () => {
-    
-    socket.emit("send-message", messageRef.current.value);
-   
-    }
-  
+    socket.emit("send-message",idRef.current.value, messageRef.current.value);
+  };
+
   return (
     <div>
-      <input type="text" ref={messageRef} />
+      <label>
+       {' message'} <input type="text" ref={messageRef} />
+      </label>
+      <label> 
+      {' send to id '} 
+        <input type="text" ref={idRef} />
+      </label>
       <button onClick={handleClick}>send</button>
       {messages.map((m) => (
         <div>{m}</div>
