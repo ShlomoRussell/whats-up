302a71a6f19df8d02d53e3ad8952a3247ae30fdb
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-05-30 01:26:00 +0300

    update SocketProvider in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,26 @@
 import React from "react";
 import Login from "./components/Login";
 import { SocketProvider } from "./context/SocketProvider";
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-import { AuthProvider, RequireAuth } from "./context/AuthContext";
-import Messages from "./components/Messages";
-import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import {  RequireAuth, useAuth } from "./context/AuthContext";
+import Chat from "./components/Chat";
 
 function App() {
+  const { user } = useAuth();
   return (
-    <SocketProvider>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/chat"
-            element={
-              <RequireAuth>
-                <Messages />
-              </RequireAuth>
-            }
-          />
-        </Routes>
-      </AuthProvider>
-    </SocketProvider>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <SocketProvider>
+              <Chat />
+            </SocketProvider>
+          </RequireAuth>
+        }
+      />
+    </Routes>
   );
 }
 
