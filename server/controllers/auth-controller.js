0f598a18e8429baa1c1718cf3004b0792a7b1e0f
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-05-29 19:23:53 +0300

    added middlewares for authenticating socket

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,25 +1,39 @@
-const { registerUser, login } = require('../logic/auth-logic');
-const ErrorModel = require('../models/error-model');
-const auth = require('express').Router();
+const { registerUser, login } = require("../logic/auth-logic");
+const ErrorModel = require("../models/error-model");
+const auth = require("express").Router();
 
-
-auth.post('/register', async (req,res,next)=> {
-    try{
-        const token = await registerUser(req.body);
-        if(!token) throw new ErrorModel(404, 'username already exist');
-        res.status(201).json(token);
-    }
-    catch(err){ next(err) }
+auth.post("/register", async (req, res, next) => {
+  try {
+    const token = await registerUser(req.body);
+    if (!token) throw new ErrorModel(404, "username already exist");
+    res.status(201).json(token);
+  } catch (err) {
+    next(err);
+  }
 });
 
-auth.post('/login', async (req,res,next)=> {
-    try{
-        const token = await login(req.body);
-        res.status(201).json(token);
-    }
-    catch(err){ next(err) }
+auth.post("/login", async (req, res, next) => {
+  try {
+    const token = await login(req.body);
+req.session.regenerate(function (err) {
+  if (err) next(err);
+
+  // store user information in session, typically a user id
+  req.session.user = req.body.username;
+
+  // save the session before redirection to ensure page
+  // load does not happen before session is saved
+  req.session.save(function (err) {
+      if (err) return next(err);
+      console.log(req.session)
+     res.status(201).json({ token: token, loggedIn: true })
+  });
 });
-
-
+   
+   
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = auth;
--- server/index.js
+++ server/index.js
@@ -3,31 +3,71 @@ const errorHandler = require("./middleware/error-handler");
 const ErrorModel = require("./models/error-model");
 const apiCtrl = require("./controllers/api-controller");
 const authctrl = require("./controllers/auth-controller");
+const cors = require("cors");
+const jwt = require('jsonwebtoken')
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
-
+const session = require("express-session");
 const server = require("http").createServer(app);
-const { Server } = require("socket.io"); 
-const io = new Server(server, { cors:'localhost:3000' })
+const { Server } = require("socket.io");
+const jsonwebtoken = require("jsonwebtoken");
 
-
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
 
+const sessionMiddleware = session({
+  secret: "changeit",
+  resave: false,
+  saveUninitialized: true,
+});
+
+app.use(sessionMiddleware);
+app.use("/auth", authctrl);
+
+app.use(function (req, res, next) {
+  try {
+    const [type, token] = req.headers.authorization.split(" ");
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    console.log(payload);
+    req.session.authorized = true;
+    next();
+  } catch (ex) {
+    res.status(401).send();
+  }
+});
 app.use("/api/users", apiCtrl);
-app.use('/auth', authctrl);
+
+app.use("*", (req, res, next) => next(new ErrorModel(404, "Route not found")));
+app.use(errorHandler);
+
+const io = new Server(server, { cors: "localhost:3000" });
+
+const wrap = (middleware) => (socket, next) =>
+  middleware(socket.request, {}, next);
+
+io.use(wrap(sessionMiddleware));
+io.use(function (socket, next) {
+  console.log('hello')
+  try {
+    const token = socket.handshake.auth.token;
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    console.log(payload);
+    next();
+  } catch (ex) {
+    next(ex)
+  }
+});
+
 io.on("connection", (socket) => {
   socket.on("send-message", (sendToID, message) => {
     console.log(message);
-    socket.to(sendToID).emit("receive-message", message);
+    socket.emit("receive-message", message);
   });
 
   console.log(`You're connect with the id:${socket.id}`);
 });
-
-app.use("*", (req, res, next) => next(new ErrorModel(404, "Route not found")));
-app.use(errorHandler);
 
 server.listen(PORT, () => console.log(`server started on port: ${PORT}`));
