76b08bf3c1400079906ad2cc6a419a188ef5faba
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-05-29 19:21:50 +0300

    deltted soe starter files

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,9 @@
-import { socket } from "./socket/connect-socket";
-import React, { createRef, useState } from "react";
-function App() {
-  const [messages, setMessages] = useState([]);
-  socket.on("receive-message", (message) => {
-    console.log(message);
-    setMessages([...messages, message]);
-  });
-  const messageRef = createRef();
-   const idRef = createRef();
-  const handleClick = () => {
-    socket.emit("send-message",idRef.current.value, messageRef.current.value);
-  };
+import React from "react";
+import Login from "./components/Login";
+import { SocketProvider } from "./context/SocketProvider";
 
-  return (
-    <div>
-      <label>
-       {' message'} <input type="text" ref={messageRef} />
-      </label>
-      <label> 
-      {' send to id '} 
-        <input type="text" ref={idRef} />
-      </label>
-      <button onClick={handleClick}>send</button>
-      {messages.map((m) => (
-        <div>{m}</div>
-      ))}
-    </div>
-  );
+function App() {
+  return <SocketProvider><Login /></SocketProvider>;
 }
 
 export default App;
