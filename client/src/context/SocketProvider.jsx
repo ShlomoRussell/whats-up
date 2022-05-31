720e6d8cd94efd439bc21761fd8f6f34ee8c0fee
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-05-31 12:50:22 +0300

    adde sendMessage function to Providr value and query with id

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -1,34 +1,37 @@
 import React, { createContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
-import authentication  from "../helpers/login.helper";
-
-
+import authentication from "../helpers/login.helper";
 
 export const SocketContext = createContext();
 
-
-
 export function SocketProvider({id, children }) {
   const [socket, setSocket] = useState(null);
-
+console.log(id)
   useEffect(() => {
     const newSocket = io("http://localhost:5782", {
       path: "/socket.io",
       transports: ["websocket"],
       secure: true,
+      query:{ id},
       auth: {
         token: authentication.token,
       },
     });
 
-      setSocket(newSocket);
+    setSocket(newSocket);
 
-    
     return () => newSocket.close();
   }, [authentication.loggedIn]);
-    
-    const {Provider}=SocketContext
-  return (
-      <Provider value={socket}> {children}</Provider>
-  );
+
+  function sendMessage(recipients, message,callback) {
+    socket.emit("send-message", recipients, message);
+    if(callback)return callback()
+  }
+  
+  const value = {
+    socket,
+    sendMessage,
+  };
+  const { Provider } = SocketContext;
+  return <Provider value={value}> {children}</Provider>;
 }
