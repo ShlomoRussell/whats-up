84a27dbdc457fc4cd20a1aeec681dda57a464a0c
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-06-02 01:24:45 +0300

    organized the Socket functions

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -1,18 +1,18 @@
 import React, { createContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
-import authentication from "../helpers/login.helper";
+import authentication from "../helpers/auth.helper";
+import { ConversationsProvider } from "./ConversationsProvider";
 
 export const SocketContext = createContext();
 
-export function SocketProvider({id, children }) {
+export function SocketProvider({ id, children }) {
   const [socket, setSocket] = useState(null);
-console.log(id)
   useEffect(() => {
     const newSocket = io("http://localhost:5782", {
       path: "/socket.io",
       transports: ["websocket"],
       secure: true,
-      query:{ id},
+      query: { id },
       auth: {
         token: authentication.token,
       },
@@ -21,17 +21,36 @@ console.log(id)
     setSocket(newSocket);
 
     return () => newSocket.close();
-  }, [authentication.loggedIn]);
+  }, []);
 
-  function sendMessage(recipients, message,callback) {
+  const [incomingMessage, setIncomingMessage] = useState();
+
+  function sendMessage(recipients, message, callback) {
     socket.emit("send-message", recipients, message);
-    if(callback)return callback()
+    if (callback) return callback();
   }
-  
+
+  useEffect(() => {
+    if (socket == null) return;
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+    socket.on("receive-message", (message) => {
+      console.log(message);
+      setIncomingMessage({ message: message, type: "received" });
+    });
+  }, [incomingMessage, setIncomingMessage, socket]);
+
   const value = {
     socket,
     sendMessage,
   };
   const { Provider } = SocketContext;
-  return <Provider value={value}> {children}</Provider>;
+  return (
+    <Provider value={value}>
+      <ConversationsProvider incomingMessage={incomingMessage}>
+        {children}
+      </ConversationsProvider>
+    </Provider>
+  );
 }
