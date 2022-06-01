c0f698f5cda4485e83f217631f19604dda3c4d72
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-06-02 01:22:41 +0300

    added useeffect to ConversationProvider

--- client/src/context/ConversationsProvider.jsx
+++ client/src/context/ConversationsProvider.jsx
@@ -1,15 +1,20 @@
-import React, { createContext, useState } from 'react'
-
+import React, { createContext, useEffect, useState } from "react";
 
 export const ConversationsContext = createContext();
 
+export function ConversationsProvider({ incomingMessage, children }) {
+  const [conversations, setConversations] = useState([]);
 
-export function ConversationsProvider({ children }) {
-    const [conversations, setConversations] = useState([]);
-    
+  useEffect(() => {
+    if (incomingMessage == null) return;
 
-    const{Provider}= ConversationsContext 
+    setConversations([...conversations, incomingMessage]);
+    console.log(conversations);
+  }, [incomingMessage]);
+
+  const { Provider } = ConversationsContext;
+
   return (
-      <Provider value={[conversations,setConversations]}>{children}</Provider>
-  )
+    <Provider value={[conversations, setConversations]}>{children}</Provider>
+  );
 }
