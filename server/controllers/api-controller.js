199942c60ee8326376837ad48fedb0a91e096d71
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-06-06 21:33:27 +0300

    added get(:contact) to get the chat for current contact

--- server/controllers/api-controller.js
+++ server/controllers/api-controller.js
@@ -1,32 +1,46 @@
-const ErrorModel = require('../models/error-model');
-const logic = require('../logic/user-logic');
-const apiCtrl = require('express').Router();
+const ErrorModel = require("../models/error-model");
+const logic = require("../logic/user-logic");
+const apiCtrl = require("express").Router();
+const dal = require("../DAL/dal");
+const jwt = require("jsonwebtoken");
 
-
-apiCtrl.get('/', async (req,res,next)=> {
-    const users = await logic.getAllUsersAsync();
-    res.json(users);
+apiCtrl.get("/", async (req, res, next) => {
+  const users = await logic.getAllUsersAsync();
+  res.json(users);
 });
 
-apiCtrl.get('/search', async (req,res,next)=> {
-    const username = req.query.username
-    const user = await logic.getUserByUsernameAsync(username);
-    if(!user) return next(new ErrorModel(404, 'id not found'));
-    res.json(user);
+apiCtrl.get("/:contact", async (req, res) => {
+  const contact = req.params.contact;
+  const currentUser = req.headers.authorization.split(" ")[1];
+
+  const { id } = jwt.decode(currentUser);
+
+  const result = await dal
+    .getAllUsersAsync()
+    .then((res) => res.find((u) => u.id === id))
+    .then((res) => res["contacts"].find((c) => c.id === contact));
+  console.log(result)
+  res.send(result);
 });
 
-
-apiCtrl.put('/:id', async (req,res,next)=> {
-    const id = req.params.id;
-    const updatedUser = req.body;
-    const user = await logic.updateUserAsync(id, updatedUser);
-    if(!user) return next(new ErrorModel(404, 'id not found'));
-    res.json(user);
+apiCtrl.get("/search", async (req, res, next) => {
+  const username = req.query.username;
+  const user = await logic.getUserByUsernameAsync(username);
+  if (!user) return next(new ErrorModel(404, "id not found"));
+  res.json(user);
 });
 
-apiCtrl.delete('/:id', async (req,res)=> {
-    await logic.deleteUserAsync(req.params.id);
-    res.sendStatus(204);
+apiCtrl.put("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  const updatedUser = req.body;
+  const user = await logic.updateUserAsync(id, updatedUser);
+  if (!user) return next(new ErrorModel(404, "id not found"));
+  res.json(user);
+});
+
+apiCtrl.delete("/:id", async (req, res) => {
+  await logic.deleteUserAsync(req.params.id);
+  res.sendStatus(204);
 });
 
 module.exports = apiCtrl;
