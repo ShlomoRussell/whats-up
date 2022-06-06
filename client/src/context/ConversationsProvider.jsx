37428858a54fcdf141958fe2814d9a6e94c938c5
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-06-06 21:30:51 +0300

    added fetch for each chat

--- client/src/context/ConversationsProvider.jsx
+++ client/src/context/ConversationsProvider.jsx
@@ -1,20 +1,37 @@
 import React, { createContext, useEffect, useState } from "react";
+import authentication from "../helpers/auth.helper";
 
 export const ConversationsContext = createContext();
 
 export function ConversationsProvider({ incomingMessage, children }) {
   const [conversations, setConversations] = useState([]);
-
+  const [currentConversation, setCurrentConversation] = useState([]);
+  const [currentContactId, setCurrentContactId] = useState("");
   useEffect(() => {
     if (incomingMessage == null) return;
 
-    setConversations([...conversations, incomingMessage]);
-    console.log(conversations);
+    setCurrentConversation([...currentConversation, incomingMessage]);
   }, [incomingMessage]);
 
-  const { Provider } = ConversationsContext;
+  useEffect(() => {
+    if (!currentContactId) return;
+    fetch(`http://localhost:5782/api/users/${currentContactId}`, {
+      headers: { Authorization: "Bearer " + authentication.token },
+    })
+      .then((res) => res.json())
+      .then((jres) =>
+        setCurrentConversation([...currentConversation, ...jres.messages])
+      );
+  }, [currentContactId]);
 
-  return (
-    <Provider value={[conversations, setConversations]}>{children}</Provider>
-  );
+  const { Provider } = ConversationsContext;
+  const value = {
+    conversations,
+    setConversations,
+    currentConversation,
+    setCurrentConversation,
+    currentContactId,
+    setCurrentContactId,
+  };
+  return <Provider value={value}>{children}</Provider>;
 }
