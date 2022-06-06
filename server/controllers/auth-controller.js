f14748e929debfeb5c0ebb65d6c4f58e54ffd171
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-06-06 12:37:48 +0300

    added the whle user to the res.json

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -20,7 +20,7 @@ auth.post("/login", async (req, res, next) => {
     const user = await login(req.body);
     if(user){
       const token = jwt.sign(req.body, process.env.JWT_SECRET_KEY);
-      res.status(201).json({ token: token, loggedIn: true, id: req.body.id });
+      res.status(201).json({ token: token, loggedIn: true, ...user });
     }
   } catch (err) {
     next(err);
