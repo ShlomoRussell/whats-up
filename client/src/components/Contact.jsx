8a30c14d9d79e21989f9a721ad1649c453ba5bcc
bearddude1 <baapteshuva@gmail.com>
2022-06-15 02:02:39 +0300

    fixed contact

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,31 +1,33 @@
-import React,{useContext} from 'react'
-import { Button } from 'react-bootstrap';
+import React, { useContext } from "react";
+import { Button } from "react-bootstrap";
 import { ConversationsContext } from "../context/ConversationsProvider";
 
-
 function Contact({ contact }) {
-  const {  setCurrentContactId }=useContext(ConversationsContext)
+  const { setCurrentContactId } = useContext(ConversationsContext);
+
   const handleClick = (e) => {
-    setCurrentContactId(contact.id)
-  }
- 
+    setCurrentContactId(contact.id);
+  };
+
   return (
-    <Button
-      onClick={handleClick}
-      variant="link"
-      className="list-group-item list-group-item-action py-3 lh-tight"
-    >
-      <div className="d-flex w-100 align-items-center justify-content-between">
-        <strong className="mb-1">{contact.username}</strong>
-        <small className="text-muted">
-          {contact.messages[contact.messages.length - 1].time}
-        </small>
-      </div>
-      <div className="col-10 mb-1 small">
-        {contact.messages[contact.messages.length - 1].message}...
-      </div>
-    </Button>
+    contact && (
+      <Button
+        onClick={handleClick}
+        variant="link"
+        className="list-group-item list-group-item-action py-3 lh-tight"
+      >
+        <div className="d-flex w-100 align-items-center justify-content-between">
+          <strong className="mb-1">{contact.username}</strong>
+          <small className="text-muted">
+            {contact.messages[contact.messages.length - 1].time}
+          </small>
+        </div>
+        <div className="col-10 mb-1 small">
+          {contact.messages[contact.messages.length - 1].message}...
+        </div>
+      </Button>
+    )
   );
 }
 
-export default Contact
+export default Contact;
