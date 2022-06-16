15ab881d14c03b89bd08da940ec2cf8e38d0d47b
bearddude1 <baapteshuva@gmail.com>
2022-06-17 00:29:02 +0300

    Created sidebarHeader with profpic and icon btns

--- client/src/components/SidebarContainer.jsx
+++ client/src/components/SidebarContainer.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { IoAdd } from "react-icons/io5";
-import { Link } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
 import Search from "./Search";
 import SidebarBody from "./SidebarBody";
+import SidebarHeader from "./SidebarHeader";
 
 function SidebarContainer() {
-  const { user } = useAuth()
-  
   return (
     <div className="d-flex flex-column align-items-stretch flex-shrink-0 w-25 bg-white ">
+      <SidebarHeader />
       <Search />
       <SidebarBody />
     </div>
---
+++ client/src/components/SidebarHeader.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { OverlayTrigger, Tooltip, Image } from "react-bootstrap";
+import { IoAdd } from "react-icons/io5";
+import { BsPersonCircle, BsThreeDots } from "react-icons/bs";
+import { VscCircleLargeOutline } from "react-icons/vsc";
+import { useState } from "react";
+function SidebarHeader() {
+  const [profilePic, setProfilePic] = useState();
+  return (
+    <div className="d-flex justify-content p-2 border-bottom">
+      <div style={{ height: "2.75rem", width: "2.75rem" }}>
+        {profilePic ? (
+          <Image
+            src={profilePic}
+            roundedCircle={true}
+            className="mh-100 mw-100"
+          />
+        ) : (
+          <OverlayTrigger
+            placement="bottom"
+            overlay={<Tooltip id={`tooltip-bottom`}>Add profile pic</Tooltip>}
+          >
+            <span className="m-1">
+              <BsPersonCircle
+                className="h-75 w-75 my-1 border-0"
+                style={{
+                  backgroundColor: "#bfc2c6",
+                  color: "white",
+                  borderRadius: "50%",
+                }}
+              />
+            </span>
+          </OverlayTrigger>
+        )}
+      </div>
+      <div className="ms-auto">
+        <OverlayTrigger
+          placement="left"
+          overlay={<Tooltip id={`tooltip-left`}>Status</Tooltip>}
+        >
+          <span className="m-1">
+            <VscCircleLargeOutline className="mx-auto mt-2" />
+          </span>
+        </OverlayTrigger>
+        <OverlayTrigger
+          placement="bottom"
+          overlay={<Tooltip id={`tooltip-bottom`}>New chat</Tooltip>}
+        >
+          <span className="m-1">
+            <IoAdd className="mx-auto mt-2" />
+          </span>
+        </OverlayTrigger>
+        <OverlayTrigger
+          placement="right"
+          overlay={<Tooltip id={`tooltip-right`}>Menu</Tooltip>}
+        >
+          <span className="m-1">
+            <BsThreeDots className="mx-auto mt-2" />
+          </span>
+        </OverlayTrigger>
+      </div>
+    </div>
+  );
+}
+
+export default SidebarHeader;
