9eb385b71d0093134b902f98f6fc602ef2728f35
bearddude1 <baapteshuva@gmail.com>
2022-06-21 00:24:31 +0300

    adde dropdown menu from three dots

---
+++ client/src/components/UserMenuDropdown.jsx
@@ -0,0 +1,40 @@
+import { Dropdown, OverlayTrigger, Tooltip } from "react-bootstrap";
+import React from "react";
+import { BsThreeDots } from "react-icons/bs";
+import { useAuth } from "../context/AuthContext";
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  <OverlayTrigger
+    placement="bottom"
+    overlay={<Tooltip id={`tooltip-bottom`}>Menu</Tooltip>}
+  >
+        <span className="m-1" onClick={(e) => {
+            e.preventDefault()
+            onClick(e)
+        }}>
+      <BsThreeDots className="mx-auto mt-2" />
+    </span>
+  </OverlayTrigger>
+));
+
+function UserMenuDropdown() {
+    const { signout } = useAuth();
+  return (
+    <Dropdown className="d-inline" autoClose="outside">
+      <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+        Custom toggle
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu align="start">
+        <Dropdown.Item eventKey="1">Red</Dropdown.Item>
+        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
+        <Dropdown.Item eventKey="3">Orange</Dropdown.Item>
+        <Dropdown.Item eventKey="1" onClick={signout}>
+          Log out
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default UserMenuDropdown;
