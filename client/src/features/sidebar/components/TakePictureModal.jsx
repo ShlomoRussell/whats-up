01acfa48a44addb82232a74e6da9e0cc2d0ae8b7
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-11-26 21:00:21 +0200

    finish TakePictureModal

--- client/src/features/sidebar/components/TakePictureModal.jsx
+++ client/src/features/sidebar/components/TakePictureModal.jsx
@@ -1,11 +1,27 @@
 import React, { useRef, useCallback, useState } from "react";
-import { Image, Modal } from "react-bootstrap";
+import { Button, Image, Modal } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import Webcam from "react-webcam";
+import { selectCurrentUser } from "../../auth/redux/authSlice";
+import { useUploadProfilePicMutation } from "../redux/sideBarApiSlice";
 
 function TakePictureModal({ _show, toggleShow, onUserMediaError }) {
   const [picture, setPicture] = useState("");
   const [show, setShow] = useState(false);
+  const { image } = useSelector(selectCurrentUser);
+  const [uploadPicture] = useUploadProfilePicMutation();
   const webcamRef = useRef();
+  const serverError = () => alert("There was an error. Please try again");
+
+  const savePicture = async () => {
+    try {
+      await uploadPicture({ img: picture, oldImgPath: image });
+    } catch (error) {
+      serverError();
+      console.log(error);
+    }
+  };
+  
   const capture = useCallback(() => {
     const pictureSrc = webcamRef.current.getScreenshot();
     console.log(pictureSrc);
@@ -33,9 +49,10 @@ function TakePictureModal({ _show, toggleShow, onUserMediaError }) {
       <Modal.Body className="m-0">
         {picture === "" ? (
           <Webcam
+            className="m-0"
             audio={false}
             height={"375px"}
-            width={"500px"}
+            width={"100%"}
             screenshotFormat="image/jpeg"
             onUserMediaError={() => {
               setShow(false);
@@ -52,14 +69,13 @@ function TakePictureModal({ _show, toggleShow, onUserMediaError }) {
           <Image src={picture} />
         )}
         {picture !== "" ? (
-          <button onClick={(e) => setPicture("")} className="btn btn-primary">
-            Retake
-          </button>
+          <Button onClick={(e) => setPicture("")}>Retake</Button>
         ) : (
-          <button onClick={capture} className="btn btn-danger">
+          <Button variant="danger" onClick={capture}>
             Capture
-          </button>
+          </Button>
         )}
+        <Button onClick={savePicture}>Save</Button>
       </Modal.Body>
     </Modal>
   );
